36aacd99231620f09c9a15ec50a35a9f7fb2456f
Adriano Ueda <adriano.ueda@gmail.com>
2019-01-16 00:17:51 -0500

    Implement unit tests by frameworks (#9)
    Implement unit tests for each framework:
    
    - .NET 4.0
    - .NET 4.5
    - .NET 4.5.2
    - .NET 4.6.2
    - .NET Core 2.0

---
+++ Maoli.Net45.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Maoli.Net45.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Maoli.Net45.Tests")]
+[assembly: AssemblyCopyright("Copyright © 2019 Adriano Ueda")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9cf2471d-1da0-4276-a247-659bbcb49511")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
