ce515fccb8f7293e90a049b1325a9b27fffdd523
Adriano Ueda <adriano.ueda@gmail.com>
2019-01-15 23:03:26 -0500

    Implement unit tests for .NET Framework 4.0.

--- Maoli.Net40.Tests/Properties/AssemblyInfo.cs
+++ Maoli.Net40.Tests/Properties/AssemblyInfo.cs
@@ -2,11 +2,11 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Maoli.Net45.Tests")]
+[assembly: AssemblyTitle("Maoli.Net40.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Maoli.Net45.Tests")]
+[assembly: AssemblyProduct("Maoli.Net40.Tests")]
 [assembly: AssemblyCopyright("Copyright © 2019 Adriano Ueda")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
