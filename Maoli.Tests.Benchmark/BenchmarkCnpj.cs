826def2c99c368f21254bfb7c0fbfc6335dd13c7
Adriano Ueda <adriano.ueda@gmail.com>
2022-08-11 08:28:01 -0400

    Refactor the internal code to use ReadOnlySpan and add benchmark tests.

---
+++ Maoli.Tests.Benchmark/BenchmarkCnpj.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Adriano Ueda. All rights reserved.
+
+namespace Maoli.Benchmark
+{
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Jobs;
+    using Maoli;
+
+    [SimpleJob(RuntimeMoniker.NetCoreApp31)]
+    [SimpleJob(RuntimeMoniker.Net60)]
+    [SimpleJob(RuntimeMoniker.Net48)]
+    [MemoryDiagnoser]
+    [MarkdownExporterAttribute.GitHub]
+    [RankColumn]
+    public class BenchmarkCnpj
+    {
+        [Benchmark]
+        public bool StrictValidCnpj() =>
+            Cnpj.Validate("63.943.315/0001-92");
+
+        [Benchmark]
+        public bool StrictInvalidCnpj() =>
+            Cnpj.Validate("32.343.315/0001-34");
+
+        [Benchmark]
+        public bool LooseValidCnpj() =>
+            Cnpj.Validate("63943315000192");
+
+        [Benchmark]
+        public bool LooseInvalidCnpj() =>
+            Cnpj.Validate("32343315/000134");
+    }
+}
---
+++ Maoli.Tests.Benchmark/Program.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Adriano Ueda. All rights reserved.
+
+namespace Maoli.Tests.Benchmark
+{
+    using BenchmarkDotNet.Running;
+
+    public class Program
+    {
+        public static void Main(
+            string[] args)
+        {
+            _ = 
+                BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+}
