32c78a1e05c625ebce999f385671bf0bda796ee8
Adriano Ueda <adriano.ueda@gmail.com>
2022-08-11 17:29:59 -0400

    Update project.

--- Maoli.Tests/test.bat
+++
@@ -1,3 +0,0 @@
-dotnet test --configuration Release --framework net5.0 /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=..\TestResults\Coverage\
-
-dotnet reportgenerator "-reports:..\TestResults\Coverage\coverage.net5.0.cobertura.xml" "-targetdir:..\TestResults\Coverage\Reports" -reportTypes:HtmlInline_AzurePipelines;htmlInline
