e72b849cd997fd33a0602154988fc879aae36849
Adriano Ueda <adriano.ueda@gmail.com>
2022-09-28 17:39:45 -0400

    Refactor the number speller to use span. (#47)
    * Refactor to use span.

--- Maoli/Spellers/NumberSpeller.cs
+++ Maoli/Spellers/NumberSpeller.cs
@@ -2,8 +2,12 @@
 
 namespace Maoli.Spellers
 {
+    using System;
     using System.Diagnostics.CodeAnalysis;
+    using System.Runtime.CompilerServices;
+#if !NET5_0_OR_GREATER
     using System.Text;
+#endif
 
     /// <summary>
     /// Defines methods for a number speller.
@@ -11,11 +15,68 @@ namespace Maoli.Spellers
     public sealed class NumberSpeller
         : INumberSpeller<int>, INumberSpeller<long>
     {
+#if !NET5_0_OR_GREATER
         private const string Conjunction = " e ";
 
         private const string SpecialName =
             "cem";
+#endif
 
+        private const string Names =
+            " " +
+            "e" +
+            " " +
+            "zero" +
+            "um" +
+            "dois" +
+            "três" +
+            "quatro" +
+            "cinco" +
+            "seis" +
+            "sete" +
+            "oito" +
+            "nove" +
+            "dez" +
+            "onze" +
+            "doze" +
+            "treze" +
+            "quatorze" +
+            "quinze" +
+            "dezesseis" +
+            "dezessete" +
+            "dezoito" +
+            "dezenove" +
+            "vinte" +
+            "trinta" +
+            "quarenta" +
+            "cinquenta" +
+            "sessenta" +
+            "setenta" +
+            "oitenta" +
+            "noventa" +
+            "cento" +
+            "duzentos" +
+            "trezentos" +
+            "quatrocentos" +
+            "quinhentos" +
+            "seiscentos" +
+            "setecentos" +
+            "oitocentos" +
+            "novecentos" +
+            "mil" +
+            "milhão" +
+            "bilhão" +
+            "trilhão" +
+            "quatrilhão" +
+            "quintilhão" +
+            "mil" +
+            "milhões" +
+            "bilhões" +
+            "trilhões" +
+            "quatrilhões" +
+            "quintilhões";
+
+#if !NET5_0_OR_GREATER
         private static readonly string[] OneNames =
             new[]
             {
@@ -45,20 +106,6 @@ namespace Maoli.Spellers
                 "noventa",
             };
 
-        private static readonly string[] HundredNames =
-            new[]
-            {
-                "cento",
-                "duzentos",
-                "trezentos",
-                "quatrocentos",
-                "quinhentos",
-                "seiscentos",
-                "setecentos",
-                "oitocentos",
-                "novecentos",
-            };
-
         private static readonly string[] AltTenNames =
             new[]
             {
@@ -72,6 +119,20 @@ namespace Maoli.Spellers
                 "dezessete",
                 "dezoito",
                 "dezenove",
+            };
+
+        private static readonly string[] HundredNames =
+            new[]
+            {
+                "cento",
+                "duzentos",
+                "trezentos",
+                "quatrocentos",
+                "quinhentos",
+                "seiscentos",
+                "setecentos",
+                "oitocentos",
+                "novecentos",
             };
 
         private static readonly string[] ThousandNames =
@@ -95,6 +156,96 @@ namespace Maoli.Spellers
                 "quatrilhões",
                 "quintilhões",
             };
+#endif
+
+        private static readonly int[] OneNameStartIndexes =
+            new[]
+            {
+                03, 04, // zero
+                07, 02, // um
+                09, 04, // dois
+                13, 04, // três
+                17, 06, // quatro
+                23, 05, // cinco
+                28, 04, // seis
+                32, 04, // sete
+                36, 04, // oito
+                40, 04, // nove
+            };
+
+        private static readonly int[] AltTenNameIndexes =
+            new[]
+            {
+                44, 03, // dez
+                47, 04, // onze
+                51, 04, // doze
+                55, 05, // treze
+                60, 08, // quatorze
+                68, 06, // quinze
+                74, 09, // dezesseis
+                83, 09, // dezessete
+                92, 07, // dezoito
+                99, 08, // dezenove
+            };
+
+        private static readonly int[] TenNamesIndexes =
+            new[]
+            {
+                044, 03, // dez
+                107, 05, // vinte
+                112, 06, // trinta
+                118, 08, // quarenta
+                126, 09, // cinquenta
+                135, 08, // sessenta
+                143, 07, // setenta
+                150, 07, // oitenta
+                157, 07, // noventa
+            };
+
+        private static readonly int[] HundredNameIndexes =
+            new[]
+            {
+                164, 05, // cento
+                169, 08, // duzentos
+                177, 09, // trezentos
+                186, 12, // quatrocentos
+                198, 10, // quinhentos
+                208, 10, // seiscentos
+                218, 10, // setecentos
+                228, 10, // oitocentos
+                238, 10, // novecentos
+            };
+
+        private static readonly int[] ThousandNameIndexes =
+            new[]
+            {
+                248, 03, // mil
+                251, 06, // milhão
+                257, 06, // bilhão
+                263, 07, // trilhão
+                270, 10, // quatrilhão
+                280, 10, // quintilhão
+            };
+
+        private static readonly int[] PluralThousandNameIndexes =
+            new[]
+            {
+                290, 03, // mil
+                293, 07, // milhões
+                300, 07, // bilhões
+                307, 08, // trilhões
+                315, 11, // quatrilhões
+                326, 11, // quintilhões
+            };
+
+        /// <summary>
+        /// Initializes a new instance of the
+        /// <see cref="NumberSpeller"/> class.
+        /// </summary>
+        public NumberSpeller()
+            : base()
+        {
+        }
 
         /// <summary>
         /// Returns the number spelled in Brazilian Portuguese.
@@ -114,6 +265,191 @@ namespace Maoli.Spellers
         {
             return this.Spell((long)number);
         }
+#if NET5_0_OR_GREATER
+
+        /// <summary>
+        /// Returns the number spelled in Brazilian Portuguese.
+        /// </summary>
+        /// <param name="number">
+        /// The number.
+        /// </param>
+        /// <returns>
+        /// The number spelled.
+        /// </returns>
+        public string Spell(
+#if NET5_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+            [NotNull] long number)
+#else
+            long number)
+#endif
+        {
+            if (number == 0L)
+            {
+                return
+                    GetOneName(0).ToString();
+            }
+
+            Span<char> span = stackalloc char[256];
+
+            var conj = Names.AsSpan().Slice(0, 3);
+
+            ReadOnlySpan<char> specialName =
+                stackalloc char[3]
+                {
+                    'c',
+                    'e',
+                    'm',
+                };
+
+            var startIndex = span.Length - 1;
+
+            var partialNumber = number;
+            var firstGroup = true;
+            var requiresConjunction = false;
+            var groupCount = 0;
+
+            do
+            {
+                var numberClass = (int)(partialNumber % 1000);
+
+                partialNumber /= 1000;
+
+                if (numberClass > 0)
+                {
+                    if (firstGroup)
+                    {
+                        firstGroup = false;
+
+                        if (numberClass <= 100 || numberClass % 100 <= 0)
+                        {
+                            requiresConjunction = true;
+                        }
+                    }
+
+                    if (groupCount > 0)
+                    {
+                        if (numberClass > 1)
+                        {
+                            Insert(
+                                span,
+                                GetPluralThousandName(groupCount - 1),
+                                ref startIndex);
+                        }
+                        else
+                        {
+                            Insert(
+                                span,
+                                GetThousandName(groupCount - 1),
+                                ref startIndex);
+                        }
+
+                        Insert(span, conj.Slice(0, 1), ref startIndex);
+                    }
+
+                    if (groupCount != 1 || numberClass != 1)
+                    {
+                        var digit3 = (int)(numberClass % 10);
+                        var digit2 = (int)((numberClass / 10) % 10);
+                        var digit1 = (int)((numberClass / 100) % 10);
+
+                        // Write ones
+                        if (digit3 > 0 && digit2 != 1)
+                        {
+                            Insert(
+                                span,
+                                GetOneName(digit3),
+                                ref startIndex);
+                        }
+
+                        // Write tens
+                        if (digit2 == 1)
+                        {
+                            Insert(
+                                span,
+                                GetAltTenName(digit3),
+                                ref startIndex);
+                        }
+                        else
+                        {
+                            if (digit2 > 0)
+                            {
+                                if (digit3 > 0)
+                                {
+                                    Insert(
+                                        span,
+                                        conj,
+                                        ref startIndex);
+                                }
+
+                                Insert(
+                                    span,
+                                    GetTenName(digit2 - 1),
+                                    ref startIndex);
+                            }
+                        }
+
+                        // Write hundreds
+                        if (digit1 > 0)
+                        {
+                            if (digit1 == 1 && digit2 + digit3 == 0)
+                            {
+                                Insert(
+                                    span,
+                                    specialName,
+                                    ref startIndex);
+                            }
+                            else
+                            {
+                                if (digit2 + digit3 > 0)
+                                {
+                                    Insert(
+                                        span,
+                                        conj,
+                                        ref startIndex);
+                                }
+
+                                Insert(
+                                    span,
+                                    GetHundredName(digit1 - 1),
+                                    ref startIndex);
+                            }
+                        }
+                    }
+
+                    if (partialNumber > 0)
+                    {
+                        if (requiresConjunction)
+                        {
+                            Insert(
+                                span,
+                                conj,
+                                ref startIndex);
+
+                            requiresConjunction = false;
+                        }
+                        else
+                        {
+                            Insert(span, conj[0], ref startIndex);
+                        }
+                    }
+                }
+
+                groupCount++;
+            }
+            while (partialNumber > 0);
+
+            var offset =
+                span[startIndex] == conj[0]
+                    ? 1
+                    : 0;
+
+            return span
+                .Slice(
+                    startIndex + offset,
+                    span.Length - startIndex - 1 - offset)
+                .ToString();
+        }
+#else
 
         /// <summary>
         /// Returns the number spelled in Brazilian Portuguese.
@@ -199,6 +535,218 @@ namespace Maoli.Spellers
                 .ToString()
                 .Trim();
         }
+#endif
+#if NET5_0_OR_GREATER
+
+        /// <summary>
+        /// Returns the one name by index.
+        /// </summary>
+        /// <param name="index">
+        /// The index.
+        /// </param>
+        /// <returns>
+        /// The name as span.
+        /// </returns>
+        internal static ReadOnlySpan<char> GetOneName(int index)
+        {
+            var names = Names.AsSpan();
+
+            return names
+                .Slice(
+                    OneNameStartIndexes[index << 1],
+                    OneNameStartIndexes[(index << 1) + 1]);
+        }
+
+        /// <summary>
+        /// Returns the ten name by index.
+        /// </summary>
+        /// <param name="index">
+        /// The index.
+        /// </param>
+        /// <returns>
+        /// The name as span.
+        /// </returns>
+        internal static ReadOnlySpan<char> GetTenName(int index)
+        {
+            var names = Names.AsSpan();
+
+            return names
+                .Slice(
+                    TenNamesIndexes[index << 1],
+                    TenNamesIndexes[(index << 1) + 1]);
+        }
+
+        /// <summary>
+        /// Returns the alternate ten name by index.
+        /// </summary>
+        /// <param name="index">
+        /// The index.
+        /// </param>
+        /// <returns>
+        /// The name as span.
+        /// </returns>
+        internal static ReadOnlySpan<char> GetAltTenName(int index)
+        {
+            var names = Names.AsSpan();
+
+            return names
+                .Slice(
+                    AltTenNameIndexes[index << 1],
+                    AltTenNameIndexes[(index << 1) + 1]);
+        }
+
+        /// <summary>
+        /// Returns the hundred name by index.
+        /// </summary>
+        /// <param name="index">
+        /// The index.
+        /// </param>
+        /// <returns>
+        /// The name as span.
+        /// </returns>
+        internal static ReadOnlySpan<char> GetHundredName(int index)
+        {
+            var names = Names.AsSpan();
+
+            return names
+                .Slice(
+                    HundredNameIndexes[index << 1],
+                    HundredNameIndexes[(index << 1) + 1]);
+        }
+
+        /// <summary>
+        /// Returns the thousand name by index.
+        /// </summary>
+        /// <param name="index">
+        /// The index.
+        /// </param>
+        /// <returns>
+        /// The name as span.
+        /// </returns>
+        internal static ReadOnlySpan<char> GetThousandName(int index)
+        {
+            var names = Names.AsSpan();
+
+            return names
+                .Slice(
+                    ThousandNameIndexes[index << 1],
+                    ThousandNameIndexes[(index << 1) + 1]);
+        }
+
+        /// <summary>
+        /// Returns the plural thousand name by index.
+        /// </summary>
+        /// <param name="index">
+        /// The index.
+        /// </param>
+        /// <returns>
+        /// The name as span.
+        /// </returns>
+        internal static ReadOnlySpan<char> GetPluralThousandName(int index)
+        {
+            var names = Names.AsSpan();
+
+            return names
+                .Slice(
+                    PluralThousandNameIndexes[index << 1],
+                    PluralThousandNameIndexes[(index << 1) + 1]);
+        }
+#endif
+#if NET5_0_OR_GREATER
+
+        /// <summary>
+        /// Inserts a string into span
+        /// using a pointer to the start of the slice.
+        /// </summary>
+        /// <param name="span">
+        /// The span.
+        /// </param>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        /// <param name="e">
+        /// The start of the slice.
+        /// </param>
+        private static void Insert(
+            Span<char> span,
+            ReadOnlySpan<char> value,
+            ref int e)
+        {
+            CopyFrom(
+                span.Slice(e - value.Length, value.Length),
+                value);
+
+            e -= value.Length;
+        }
+
+        /// <summary>
+        /// Inserts a char into span
+        /// using a pointer to the start of the slice.
+        /// </summary>
+        /// <param name="span">
+        /// The span.
+        /// </param>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        /// <param name="e">
+        /// The start of the slice.
+        /// </param>
+        private static void Insert(
+            Span<char> span,
+            char value,
+            ref int e)
+        {
+            CopyFrom(
+                span.Slice(e - 1, 1),
+                value);
+
+            e--;
+        }
+
+        /// <summary>
+        /// Copies a char to the span.
+        /// </summary>
+        /// <param name="span">
+        /// The span.
+        /// </param>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        private static void CopyFrom(
+            Span<char> span,
+            char value)
+        {
+            if (span.Length > 0)
+            {
+                span[0] = value;
+            }
+        }
+
+        /// <summary>
+        /// Copies a string value to the span.
+        /// </summary>
+        /// <param name="span">
+        /// The span.
+        /// </param>
+        /// <param name="value">
+        /// The value.
+        /// </param>
+        private static void CopyFrom(
+            Span<char> span,
+            ReadOnlySpan<char> value)
+        {
+            for (var i = 0; i < span.Length; i++)
+            {
+                if (i > value.Length)
+                {
+                    break;
+                }
+
+                span[i] = value[i];
+            }
+        }
+#else
 
         /// <summary>
         /// Returns the spell of last 3 digits of a number.
@@ -293,5 +841,6 @@ namespace Maoli.Spellers
                 sb.Append(OneNames[digit3]);
             }
         }
+#endif
     }
 }
